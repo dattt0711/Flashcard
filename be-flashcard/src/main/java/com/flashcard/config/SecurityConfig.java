1f8112f8a99aa01ef3e7bb8395bb6ca4cad175d6
datpd7 <datpd7@fpt.com>
2026-01-15 08:40:29 +0700

    cors

--- be-flashcard/src/main/java/com/flashcard/config/SecurityConfig.java
+++ be-flashcard/src/main/java/com/flashcard/config/SecurityConfig.java
@@ -58,7 +58,11 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000"));
+        configuration.setAllowedOrigins(Arrays.asList(
+                "http://localhost:3000",
+                "https://studies.group",
+                "https://www.studies.group"
+        ));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
